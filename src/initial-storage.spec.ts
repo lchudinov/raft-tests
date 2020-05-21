3f3b60b5fce79b14584049d9efb1aa692eb18fcd
Leonty Chudinov <lchudinov@rocketsoftware.com>
2020-05-21 14:26:35 +0500

    Initial commit
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

---
+++ src/initial-storage.spec.ts
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import { expect } from 'chai';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+
+xdescribe('Initial Storage', () => {
+  const req = request('https://localhost:10010');
+  let cookie: string[];
+  const key = '123';
+  const value = '456';
+
+  it('should get main page', async () => {
+    const res = await req.get('/ui/v1/zlux/ZLUX/plugins/org.zowe.zlux.bootstrap/web/').send();
+    expect(res.status).to.equal(200);
+  });
+  
+  it('should authenticate', async () => {
+    const res = await req.post('/ui/v1/zlux/auth').send({
+      username: "1",
+      password: ""
+    });
+    cookie = res.header['set-cookie'];
+    expect(res.status).to.equal(200);
+  });
+  
+  it('should put a key/value into cluster storage', async () => {
+
+    const res = await req
+    .put(`/ui/v1/zlux/ZLUX/plugins/org.zowe.zlux.sample.angular/services/hello/_current/${key}`)
+    .set('Cookie', cookie)
+    .send({value});
+    expect(res.status).to.equal(200);
+  });
+  
+  it('should get correct value from cluster storage', async () => {
+    const res = await req
+    .get(`/ui/v1/zlux/ZLUX/plugins/org.zowe.zlux.sample.angular/services/hello/_current/${key}`)
+    .set('Cookie', cookie)
+    .send();
+    expect(res.status).to.equal(200);
+    expect(res.body.value).to.equal(value);
+  });
+});
